46c61367df8fc5728ab56c73743e8b73594256e5
StuartCox3107 <coxstuart@hotmail.com>
2020-09-10 16:51:19 +0000

    revert to lesson 4

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
 import os
 from flask import Flask
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
 
-if os.path.exists("env.py"):
-    import env
-
-app.config["MONGO_DBNAME"] = ("task_manager")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 app = Flask(__name__)
 
-mongo = PyMongo(app)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
 
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+def hello():
+    return 'Hello World ...again'
 
 
 if __name__ == '__main__':
