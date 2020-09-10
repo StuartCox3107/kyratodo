d751f7382f95b7b5a13e8291088dfbfa1321fa8f
StuartCox3107 <coxstuart@hotmail.com>
2020-09-10 16:41:53 +0000

    push

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
 
+app.config["MONGO_DBNAME"] = ("task_manager")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 app = Flask(__name__)
 
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
